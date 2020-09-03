62a15f211b6c373a2df9b6b68a5b7f90690f686d
Arzl James <70579069+arzljames3@users.noreply.github.com>
2020-09-03 20:45:35 +0800

    Add files via upload

--- home.js
+++ home.js
@@ -4,13 +4,24 @@ const first = document.querySelector(".first");
 const second = document.querySelector(".second");
 const third = document.querySelector(".third");
 const secondary = document.querySelector(".second-nav");
-const section = document.querySelector(".section");
+const nav = document.querySelector("nav");
 
 burgerMenu.addEventListener("click", () => {
   secondary.classList.toggle("nav-active");
-  section.classList.toggle("section-active");
   first.classList.toggle("first-active");
   second.classList.toggle("second-active");
   third.classList.toggle("third-active");
   burgerMenu.classList.toggle("active-burger");
 });
+
+function navChange() {
+  if (this.scrollY > this.innerHeight) {
+    nav.classList.add("navbar-active");
+    secondary.classList.add("second-nav-active");
+  } else {
+    nav.classList.remove("navbar-active");
+    secondary.classList.remove("second-nav-active");
+  }
+}
+
+window.addEventListener("scroll", navChange);
